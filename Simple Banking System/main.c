aadce40f7589f36b146c2f221cdcd947e7263e51
OMKAR <nizypher0@gmail.com>
2023-04-11 17:57:09 +0530

    Update main.c
    prototyping

---
+++ Simple Banking System/main.c
@@ -0,0 +1,27 @@
+// Simple Banking System 
+/*
+	Things the should be able to do:
+	- create an new account
+	- login with thein id and password
+	- check balance
+	- deposit money
+	- withdraw money
+	- transfer money
+
+*/
+#include <stdio.h>
+
+
+void run();
+void login();
+void menu();
+void showBalance();
+void depositMoney();
+void withdrawMoney();
+void transferMoney();
+
+int main() {
+
+
+	return 0;
+}
